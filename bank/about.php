f46dc26a924b9a7518cc81c8287f0cae97a6c910
Abhishek pandey <50267963+abhis0858@users.noreply.github.com>
2021-08-19 19:50:10 +0530

    Added code from local

---
+++ bank/about.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sparks Donation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="about.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+    <style font-family: 'Montserrat' , sans-serif;></style>
+
+
+</head>
+
+<body>
+    <header class="header" style="margin-top: 0px;">
+        <h3 style="margin-right:10 px; margin-top: 20px;"><a href="about.php" class="logo">ABOUT US</a></h3>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        <ul class="menu">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="about.php" style="color: gold; font-size: larger;">ABOUT US</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="gallery.php">GALLERY</a></li>
+        </ul>
+
+
+        <div class="welcome" style="position: relative;">
+            <p style="color: white; text-align: center;"> <br>
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non mollitia nihil, quo quaerat reiciendis molestias, eius, sunt consequatur necessitatibus blanditiis dolorem nesciunt ullam. Officiis ex autem veniam mollitia vitae ut blanditiis, recusandae impedit omnis qui! Accusamus, iusto ex ipsam labore asperiores sequi modi doloribus rerum esse sint odit repellendus minus officiis quo, consectetur debitis tempora. Sunt, consequuntur repellendus. In ut quo libero quaerat sunt ipsum officiis inventore esse, optio vero exercitationem ullam! Natus omnis tenetur expedita vel iste rem aperiam placeat facere ratione ipsum repellat, nostrum sint atque incidunt recusandae modi saepe adipisci. Qui quibusdam veritatis alias dolorum nesciunt corrupti! </p>
+            <br>
+            <h3>
+                <p>Save a little for a better Tomorrow</p>
+            </h3>
+
+            <br></p>
+
+          
+
+        </div>
+    </header>
+
+
+</body>
+
+</html>
---
+++ bank/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sparks Donation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+    <style font-family: 'Montserrat' , sans-serif;></style>
+
+
+</head>
+
+<body>
+    <header class="header" style="margin-top: 0px;">
+        <div class="heading">
+            <h4 style="margin-left: 5px; margin-top: 20px;"><a href="index.php" class="logo">Abhishek Banking</a>
+            </h4>
+        </div>
+
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        <ul class="menu">
+            <li><a href="index.php" class="active">HOME</a></li>
+            <li><a href="about.php">ABOUT US</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="gallery.php">GALLERY</a></li>
+        </ul>
+
+
+        <div class="welcome" style="position: relative;">
+            <h3 style="color:gold;"><br>Save Today For Tommorow</h3>
+            <a href="users.php" target="blank">View All Users</a><br>
+            <a href="transfer_money.php">Transfer Money</a><br>
+            <a href="transactionhistory.php">Transaction History</a>
+
+        </div>
+    </header>
+
+
+</body>
+
+</html>
